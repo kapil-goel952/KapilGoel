309a22b95691294d0c022a6ccafdd9d5ed532860
kapil-goel952 <goelk849@gmail.com>
2026-08-03 23:23:42 +0530

    upated the documentation

--- src/Components/about/AboutHeroSection.jsx
+++ src/Components/about/AboutHeroSection.jsx
@@ -1,3 +1,4 @@
+// dfghjkrtyuixcvbnmcvbnmasdfghjkjhgfdfgh
 // about page 
 import React from "react";
 import { Link } from "react-router-dom";
--- src/Components/about/ToolBox.jsx
+++ src/Components/about/ToolBox.jsx
@@ -57,4 +57,4 @@ const Toolbox = () => {
 
 export default Toolbox;
 
-// dfghjkrtyuixcvbnmcvbnm
+// dfghjkrtyuixcvbnmcvbnmasdfghjkjhgfdfgh
